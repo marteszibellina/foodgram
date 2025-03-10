17550ca92098b56c11de38bb6169b7c41d66d0fd
marteszibellina <valerababahov@yandex.ru>
2025-03-11 00:06:09 +0300

    test

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -13,9 +13,9 @@ class RecipeFilter(filters.FilterSet):
         to_field_name='slug',
         queryset=Tag.objects.all(),)
     is_in_shopping_cart = filters.BooleanFilter(
-        method='filter_is_in_shopping_cart', label='В списке покупок')
+        method='filter_is_in_shopping_cart', field_name='shoppingcart', label='В списке покупок')
     is_favorited = filters.BooleanFilter(
-        method='filter_is_favorited', label='В избранном')
+        method='filter_is_favorited', field_name='favorite', label='В избранном')
 
     class Meta:
         """Мета-класс фильтра"""
@@ -29,14 +29,14 @@ class RecipeFilter(filters.FilterSet):
 
     def filter_is_favorited(self, queryset, name, value):
         """Фильтр по избранному"""
-        if value:
-            return queryset.filter(favorite__isnull=False).distinct()
+        if self.request.user.is_authenticated:
+            return queryset.filter(favorite__user=self.request.user)
         return queryset
 
     def filter_is_in_shopping_cart(self, queryset, name, value):
         """Фильтр по списку покупок"""
-        if value:
-            return queryset.filter(shoppingcart__isnull=False).distinct()
+        if self.request.user.is_authenticated:
+            return queryset.filter(shoppingcart__user=self.request.user)
         return queryset
 
 
